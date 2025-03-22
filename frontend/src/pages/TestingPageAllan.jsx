5b51b159d3266694e3b3fba69cb9d345b8c513c5
allanwono <allan.wono@gmail.com>
2025-03-22 16:01:04 +0530

    full width component

--- frontend/src/pages/TestingPageAllan.jsx
+++ frontend/src/pages/TestingPageAllan.jsx
@@ -4,6 +4,7 @@ import InclusionsSection from "../components/Inclusions/inclusionsSection";
 import InfoBox from "../components/BlackInfoBox/InfoBox";
 import TestimonialsSection from "../components/Testimonials/TestimonialsSection";
 import MortgageSection from "../components/Mortgage/MortgageSection";
+import FullWidthInfoCard from "../components/FullWidthCard/FullWidthInfoCard";
 
 const TestingPageAllan = () => {
   const infoData = [
@@ -22,6 +23,23 @@ const TestingPageAllan = () => {
     { title: "Ownership Perks", description: "Income, growth, and residency." },
   ];
 
+  const sectionsData = [
+    {
+      image: "https://dummyimage.com/600x200/000/fff", // Replace with actual image URL
+      heading: "Why wait to make high ROI from Real-Estate Investments in UAE?",
+      description:
+        "Select from our highly curated opportunistic lower than market price real-estate deals to instantly start making highest monthly ROI immediately.",
+      buttonText: "Connect with us",
+    },
+    {
+      image: "https://dummyimage.com/600x200/000/fff",
+      heading: "Invest in Prime Locations Today!",
+      description:
+        "Unlock exclusive real estate deals in the most sought-after locations. Don't miss out on lucrative investment opportunities.",
+      buttonText: "Learn More",
+    },
+  ];
+
   return (
     <div>
       <TestimonialsSection />
@@ -31,6 +49,7 @@ const TestingPageAllan = () => {
         <InfoBox title="Why purchase a property in Dubai?" items={infoData} />
       </div>
       <MortgageSection />
+      <FullWidthInfoCard sections={sectionsData} />
     </div>
   );
 };
