ba9eb8be53f326d077d44b4f4581b87e3068172b
AiwinrajWono <wono.uiuxdev.intern03@gmail.com>
2025-03-22 15:16:26 +0530

    Added button layers

--- frontend/src/components/TextCard/TextCard.jsx
+++ frontend/src/components/TextCard/TextCard.jsx
@@ -2,11 +2,9 @@ import { ArrowUpRight } from "lucide-react";
 
 const TextCard = ({ title, description, link }) => {
   return (
-    <a
-      href={link}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="flex justify-between items-center p-4 bg-white rounded-xl shadow-md hover:shadow-lg transition cursor-pointer w-80"
+    <button
+    
+      className="flex justify-between w-full items-start text-start p-4 bg-primary rounded-xl shadow-md hover:shadow-lg transition cursor-pointer border-[0.3px] border-gray-200"
     >
       <div>
         <h3 className="text-lg font-semibold">{title}</h3>
@@ -15,7 +13,7 @@ const TextCard = ({ title, description, link }) => {
       <div className="p-2 bg-gray-100 rounded-full">
         <ArrowUpRight className="w-5 h-5 text-gray-600" />
       </div>
-    </a>
+    </button>
   );
 };
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import Dubai1 from "../assets/media/videos/dubai-main.webm";
+import TextCard from "../components/TextCard/TextCard";
 
 const Home = () => {
+  const publishers = [
+    {id : 1, publisher : 'Forbes'},
+    {id : 2, publisher : 'Fast Company'},
+    {id : 3, publisher : 'Entrepreneur'},
+  ]
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-16">
       {/* Video Section */}
-      <div className="h-[60vh] border-[0.5px] border-gray-300 rounded-lg overflow-hidden">
+      <div className="h-[60vh] relative border-[0.5px] border-gray-300 rounded-lg overflow-hidden">
         <video
           src={Dubai1}
           controls
@@ -14,11 +20,44 @@ const Home = () => {
           playsInline
           className="w-full h-full object-cover rounded-lg pointer-events-none"
         />
+
+        <div className="absolute inset-0 bg-black/10  grid grid-cols-2 rounded-lg">
+          <div className="w-full p-4">
+            <p className="text-hero text-white">
+              An Easy BRIDG for<br/> Non-Residents to invest in High ROI Real Estate
+              in UAE
+            </p>
+          </div>
+          <div className="w-full"></div>
+          <div className="w-full flex gap-4 p-4 justify-start items-end">
+            {publishers.map((item)=>(
+              <p key={item.id} className="text-white">{item.publisher}</p>
+            ))}
+          </div>
+          <div className="w-full"></div>
+        </div>
       </div>
 
       {/* Button Section */}
-      <div className="flex justify-between items-center">
-      
+      <div className="flex gap-4  items-center">
+        <TextCard
+          title={"Connect With Us"}
+          description={
+            "Contact us for FREE advice to invest in UAE from any counrty"
+          }
+        />
+        <TextCard
+          title={"Connect With Us"}
+          description={
+            "Contact us for FREE advice to invest in UAE from any counrty"
+          }
+        />
+      </div>
+
+      {/* Text Section */}
+      <div>
+        <p className="text-headline">Starting from finding the highest ROI Real Estate Property for investment
+        to mortgage if required to monthly property maintenance and monthly</p>
       </div>
     </div>
   );
