8e281b51ac02c75396aa069660d934e8e2807a1f
sankalp-wono <sankalp.wono@gmail.com>
2025-04-23 15:33:19 +0530

    set up routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,15 @@ import express from "express"
 import cors from "cors"
 import { corsConfig } from "./config/corsConfig.js"
 const app = express()
+import { config } from "dotenv"
+
+config()
 
 app.use(cors(corsConfig));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
