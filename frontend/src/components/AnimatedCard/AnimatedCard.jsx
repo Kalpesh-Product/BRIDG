3ac5281a31c6d0c02ccf7380c93fb2c4735cfcb6
muskan-wono <muskan.wono@gmail.com>
2025-04-25 19:25:33 +0530

    Added check icon

--- frontend/src/components/AnimatedCard/AnimatedCard.jsx
+++ frontend/src/components/AnimatedCard/AnimatedCard.jsx
@@ -5,6 +5,8 @@ import endToEndBridg from "../../assets/end-to-end-bridg.png"
 import resident from "../../assets/resident_mortgages.webp"
 import nonResident from "../../assets/non_resident_mortgages.webp"
 import equity from "../../assets/equity_mortgages.webp"
+import { BiCheck } from "react-icons/bi";
+import { useLocation } from "react-router-dom";
 
 const imageMap = {
   geography,
@@ -16,8 +18,11 @@ const imageMap = {
   default: "",
 };
 
-const AnimatedCard = ({ imageSrc, title, description }) => {
+const AnimatedCard = ({ imageSrc, title, description,page }) => {
   const resolvedImage = imageMap[imageSrc] || imageMap["default"];
+  const location = useLocation()
+  console.log(location)
+
 
   return (
     <div className="bg-white rounded-xl shadow-md text-start flex flex-col justify-center p-6 w-full lg:w-full h-full gap-4">
@@ -33,8 +38,9 @@ const AnimatedCard = ({ imageSrc, title, description }) => {
         <h3 className="text-lg md:text-lg font-semibold leading-snug">{title}</h3>
         {Array.isArray(description)? <ul className="mt-1 space-y-1">
           {description?.map((text, i) => (
-            <li key={i} className="text-gray-500 text-sm md:text-base">
-              {text}
+            <li key={i} className="flex gap-1">
+               <BiCheck/>
+              <p className="text-gray-500 text-sm md:text-base">{text}</p>
             </li>
           ))}
         </ul>:
