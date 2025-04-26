dae3c3162a753fd4731aa63077a1e64bd9143cc7
AiwinrajWono <wono.uiuxdev.intern03@gmail.com>
2025-04-26 15:27:53 +0530

    Fix in partnership

--- frontend/src/pages/PartnerShips.jsx
+++ frontend/src/pages/PartnerShips.jsx
@@ -72,15 +72,18 @@ export default function PartnerShip() {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 w-full">
         <div className="flex flex-col justify-start">
           <ReactFitty className="text-headline md:text-hero flex w-full text-justify">
-            Partner with us to activate<br/> passive high secondary income.
+            Partner with us to activate
+            <br /> passive high secondary income.
           </ReactFitty>
 
-          <ReactFitty className="text-base mt-8">
-            We offer a discreet, end-to-end partnership model designed <br/> for
-            professionals and high-net-worth  individuals  seeking <br/> secure and
-            scalable passive income through premium <br/> real estate opportunities in
-            the UAE.
-          </ReactFitty>
+          <div className="text-base mt-8 leading-8">
+            <ReactFitty className="">
+              We offer a discreet, end-to-end partnership model designed <br />{" "}
+              for professionals and high-net-worth individuals seeking <br />{" "}
+              secure and scalable passive income through premium <br /> real
+              estate opportunities in the UAE.
+            </ReactFitty>
+          </div>
         </div>
 
         <form onSubmit={handleSubmit(onSubmit)}>
@@ -247,7 +250,14 @@ export default function PartnerShip() {
             </div>
 
             <div className="col-span-2">
-              <PrimaryButton externalStyles={"col-span-2 w-full"} type={"submit"} disabled={isPartnershipPending} title={`${isPartnershipPending ? <Spinner /> : "PARTNER WITH US"}`} />
+              <PrimaryButton
+                externalStyles={"col-span-2 w-full"}
+                type={"submit"}
+                disabled={isPartnershipPending}
+                title={`${
+                  isPartnershipPending ? <Spinner /> : "PARTNER WITH US"
+                }`}
+              />
             </div>
           </div>
         </form>
