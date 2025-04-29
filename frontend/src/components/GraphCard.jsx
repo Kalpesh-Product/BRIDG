8aa516033220d325dea05ce0d40bb7a2fb1fcf73
AiwinrajWono <wono.uiuxdev.intern03@gmail.com>
2025-04-29 19:26:17 +0530

    Real estate changes

--- frontend/src/components/GraphCard.jsx
+++ frontend/src/components/GraphCard.jsx
@@ -5,7 +5,7 @@ const GraphCard= ({data,reverse}) => {
 
   return (
     <div className="flex flex-col items-center gap-20">
-          <div className={`flex flex-col md:flex-row items-start gap-4 ${reverse ? 'md:flex-row-reverse' : ''}`}>
+          <div className={`flex flex-col md:flex-row items-start gap-10 md:gap-4 lg:gap-4 ${reverse ? 'md:flex-row-reverse' : ''}`}>
             <div className='h-[50%] w-full  md:w-[40%] lg:w-[50%] overflow-hidden'>
             <img src={data.imageSrc} alt="image" className='h-full w-full object-cover'/>
             </div>
--- frontend/src/pages/RealEstate.jsx
+++ frontend/src/pages/RealEstate.jsx
@@ -3,18 +3,18 @@ import { ReactFitty } from "react-fitty";
 import WorldMap from "../assets/media/images/world-map-final.png";
 import GraphCard from "../components/GraphCard";
 
-import totalSalesTransactions from '../assets/graphImages/total-sales-transactions.png'
-import totalSalesBreakdown from '../assets/graphImages/sales-breakdown.png'
-import totalSalesVolume from '../assets/graphImages/sales-volume.png'
-import avgSalesPrice from '../assets/graphImages/avg-png.png'
-import buyerProfile from '../assets/graphImages/bh-buy-final.png'
-import cashVsMortgage from '../assets/graphImages/bh-mortage.png'
-import transaction from '../assets/graphImages/record-final.png'
+import totalSalesTransactions from "../assets/graphImages/total-sales-transactions.png";
+import totalSalesBreakdown from "../assets/graphImages/sales-breakdown.png";
+import totalSalesVolume from "../assets/graphImages/sales-volume.png";
+import avgSalesPrice from "../assets/graphImages/avg-png.png";
+import buyerProfile from "../assets/graphImages/bh-buy-final.png";
+import cashVsMortgage from "../assets/graphImages/bh-mortage.png";
+import transaction from "../assets/graphImages/record-final.png";
 import { BiCheck } from "react-icons/bi";
- 
+import useIsMobile from "../hooks/useIsMobile";
 
 const RealEstate = () => {
-  const salesData =[
+  const salesData = [
     {
       title: "TOTAL SALES TRANSACTIONS",
       imageSrc: totalSalesTransactions,
@@ -23,20 +23,20 @@ const RealEstate = () => {
         "2024 Dubai registered an unprecedented 168,405 sales transactions.",
         "Valued at AED 423.36 billion, positioning itself as a Global Real Estate Powerhouse.",
         "The impressive 30% year-on-year (YoY) growth in transaction value and 40% increase in transaction volume underscore the sector's exceptional performance amid surging investor confidence and robust demand.",
-        "This reflects the increasing participation of mid-market and first-time buyers and indicates a more balanced market where affordability plays a key role alongside luxury demand."
-      ]
+        "This reflects the increasing participation of mid-market and first-time buyers and indicates a more balanced market where affordability plays a key role alongside luxury demand.",
+      ],
     },
     {
       title: "APARTMENT SALES IN DUBAI",
       imageSrc: totalSalesVolume,
       items: [
         "2024, off-plan apartment transactions reached an all-time high of 94,455, growing substantially compared to 56,146 in 2023. ",
-         "2024, the secondary market also saw an increase, rising to 41,984 transactions, up from 36,153 in 2023.",
-         "Apartments have consistently outperformed villas in transaction volumes over the years.",
-         "The preference for apartments can be attributed to their relatively affordable ticket size, making them more accessible to a larger pool of buyers.",
-         "In 2024, 45% of all apartment transactions were for 1 bedroom units, with an average price of AED 1,703 per sq ft.",
-         "Over 70% of the apartment sales were for properties priced below AED 2 million, reflecting strong demand for affordable living spaces driven by both end-users and investors."
-      ]
+        "2024, the secondary market also saw an increase, rising to 41,984 transactions, up from 36,153 in 2023.",
+        "Apartments have consistently outperformed villas in transaction volumes over the years.",
+        "The preference for apartments can be attributed to their relatively affordable ticket size, making them more accessible to a larger pool of buyers.",
+        "In 2024, 45% of all apartment transactions were for 1 bedroom units, with an average price of AED 1,703 per sq ft.",
+        "Over 70% of the apartment sales were for properties priced below AED 2 million, reflecting strong demand for affordable living spaces driven by both end-users and investors.",
+      ],
     },
     {
       title: "BUYER PROFILE",
@@ -44,51 +44,50 @@ const RealEstate = () => {
       items: [
         "The 2024 uptick in investor activity aligns with the recovery in global markets, the launch of high-demand off-plan projects, and the growing perception of Dubai as a stable investment hub.",
         "Investors have consistently represented the majority of buyers from 2019 to 2024.",
-        "This resurgence in investor activity highlights the growing interest in Dubai's real estate market as a lucrative investment avenue."
-       ]
+        "This resurgence in investor activity highlights the growing interest in Dubai's real estate market as a lucrative investment avenue.",
+      ],
     },
     {
       title: "CASH Vs MORTGAGE BUYING",
       imageSrc: cashVsMortgage,
       items: [
         "Mortgage buyers have traditionally dominated transactions in the past, but over the past two years, cash buyers have emerged in majority because interest rates have gone up.",
-"This shift has been driven by an increasing influx of high-net- worth individuals and global investors who favour cash purchases to expedite transactions and eliminate financing costs.",
-"In 2023, the disparity was particularly striking, with mortgage buyers accounting for only 35% of transactions compared to 65% for cash buyers.",
-"However, this gap has narrowed significantly in 2024, resulting in an almost equal split between the two groups."
-       ]
+        "This shift has been driven by an increasing influx of high-net- worth individuals and global investors who favour cash purchases to expedite transactions and eliminate financing costs.",
+        "In 2023, the disparity was particularly striking, with mortgage buyers accounting for only 35% of transactions compared to 65% for cash buyers.",
+        "However, this gap has narrowed significantly in 2024, resulting in an almost equal split between the two groups.",
+      ],
     },
   ];
 
   const transactionData = [
     "In 2024, Dubai recorded a remarkable 528,476 total rental transactions, valued at AED 44.23 billion, underscoring the city's thriving residential rental market.",
-"This milestone reflects a balanced and maturing market, with both tenants and landlords showing a strong preference for stability and long—term planning.",
-"In 2024, renewal contracts accounted for 60% of all rental agreements, maintaining the upward trend observed in recent years.",
-"This highlights tenants' increasing inclination to lock in favourable rental terms and avoid the inconvenience of frequent relocations.",
-"For landlords, these renewals provide the advantage of reduced void periods, ensuring steady income streams in a gradually stabilising rental market."
-
-  ]
+    "This milestone reflects a balanced and maturing market, with both tenants and landlords showing a strong preference for stability and long—term planning.",
+    "In 2024, renewal contracts accounted for 60% of all rental agreements, maintaining the upward trend observed in recent years.",
+    "This highlights tenants' increasing inclination to lock in favourable rental terms and avoid the inconvenience of frequent relocations.",
+    "For landlords, these renewals provide the advantage of reduced void periods, ensuring steady income streams in a gradually stabilising rental market.",
+  ];
 
   const countries = [
     "India",
     "UK",
     "China",
-    "Saudi Arabia", 
+    "Saudi Arabia",
     "Russia",
-  "Italy" ,
+    "Italy",
     "UAE",
-  "France",
-  "Poland",
-    "USA"
-  ]
- 
-const countriesData = [
-  "India and the UK have consistently dominated the buyer charts, maintaining their positions as the top two nationalities purchasing properties.",
-"Followed by China, Saudi Arabia, Russia, Italy and UAE themselves in the top 7 nationalities purchasing properties.",
-"Their sustained interest highlights the enduring appeal of Dubai's real estate market for these demographics.",
-"Meanwhile, France, Poland & United States complete the 10 positions, replacing. This change underscores Dubai's growing allure among diverse European markets."
+    "France",
+    "Poland",
+    "USA",
+  ];
 
-]
-  
+  const countriesData = [
+    "India and the UK have consistently dominated the buyer charts, maintaining their positions as the top two nationalities purchasing properties.",
+    "Followed by China, Saudi Arabia, Russia, Italy and UAE themselves in the top 7 nationalities purchasing properties.",
+    "Their sustained interest highlights the enduring appeal of Dubai's real estate market for these demographics.",
+    "Meanwhile, France, Poland & United States complete the 10 positions, replacing. This change underscores Dubai's growing allure among diverse European markets.",
+  ];
+  const isMobile = useIsMobile()
+
   return (
     <div className="flex flex-col gap-10 md:gap-12 lg:gap-16">
       <ReactFitty className="font-semibold">
@@ -97,7 +96,7 @@ const countriesData = [
 
       {/* <div className="h-[30vh] md:h-[60vh] lg:h-[80vh] w-full flex flex-col gap-4">
         {/* <ReactFitty className="text-headline">TOP 10 COUNTRIES INVESTING IN DUBAI</ReactFitty> */}
-        {/* <h2 className="text-display">TOP 10 CROSSBOARDER INVESTORS COUNTRIES</h2> 
+      {/* <h2 className="text-display">TOP 10 CROSSBOARDER INVESTORS COUNTRIES</h2> 
         <div className="h-full w-full p-4 border border-gray-300 rounded-lg bg-white">
           <img
             src={WorldMap}
@@ -107,44 +106,58 @@ const countriesData = [
         </div>
       </div> */}
 
-      <GraphCard data={salesData[0]} reverse={true}/>
+      <GraphCard data={salesData[0]} reverse={true} />
 
       <hr />
-      <div className='h-full w-full overflow-hidden'>
-            <img src={totalSalesBreakdown} alt="image" className='h-full w-full object-cover'/>
-            </div>
+      <div className="h-full w-full overflow-hidden">
+        <img
+          src={totalSalesBreakdown}
+          alt="image"
+          className="h-full w-full object-cover"
+        />
+      </div>
 
-            <hr />
-            <GraphCard data={salesData[1]} reverse={false}/>
-            <hr />
-     <div>
-       <ReactFitty className="text-headline">
-      AVERAGE SALES PRICE PER SQ FT (AED)
-      </ReactFitty>
-      <div className='lg:h-full w-full overflow-hidden'>
-            <img src={avgSalesPrice} alt="image" className='h-full w-full object-cover'/>
-            </div>
-     </div>
-     
-     <hr />
-     <GraphCard data={salesData[2]} reverse={true}/>
-     <hr />
-     <GraphCard data={salesData[3]} reverse={false}/>
-     <hr />
+      <hr />
+      <GraphCard data={salesData[1]} reverse={false} />
+      <hr />
+      <div>
+        {!isMobile ? (
 
-     <div className="flex flex-col gap-4" >
-        {/* <ReactFitty className="text-headline">TOTAL RENTAL TRANSACTIONS</ReactFitty> */}
-        <ReactFitty >TOTAL RENTAL TRANSACTIONS</ReactFitty>
-        {
-          transactionData.map((data)=>
+        <ReactFitty className="text-headline">
+          AVERAGE SALES PRICE PER SQ FT (AED)
+        </ReactFitty>
+        ) : (
+          <ReactFitty className="text-headline">
+          AVERAGE SALES PRICE <br/> PER SQ FT (AED)
+        </ReactFitty>
+        )}
+        <div className="lg:h-full w-full overflow-hidden">
+          <img
+            src={avgSalesPrice}
+            alt="image"
+            className="h-full w-full object-cover"
+          />
+        </div>
+      </div>
+
+      <hr />
+      <GraphCard data={salesData[2]} reverse={true} />
+      <hr />
+      <GraphCard data={salesData[3]} reverse={false} />
+      <hr />
+
+      <div className="flex flex-col gap-4">
+        <ReactFitty className="font-semibold">
+          TOTAL RENTAL TRANSACTIONS
+        </ReactFitty>
+        {transactionData.map((data) => (
           <div className="flex gap-2 items-start">
             <div>
-            <BiCheck size={20}/>
+              <BiCheck size={20} />
             </div>
             <p className="text-subtitle">{data}</p>
           </div>
-        )
-        }
+        ))}
         <div className="h-full w-full p-4 bg-white">
           <img
             src={transaction}
@@ -155,39 +168,45 @@ const countriesData = [
       </div>
       <hr />
 
-        <div className="flex flex-col gap-4">
-         <div className="h-[30vh] md:h-[60vh] lg:h-full w-full flex flex-col gap-4">
-        <ReactFitty className="text-headline">TOP 10 CROSSBOARDER INVESTORS COUNTRIES 
-        </ReactFitty>
-         <div className="h-full w-full p-4 border border-gray-300 rounded-lg bg-white">
-          <img
-            src={WorldMap}
-            alt="world-map"
-            className="w-full h-full object-contain   rounded-lg"
-          />
-        </div>
-      </div>
-            <ul className="">
-              {
-                countries.map((country,index)=>
-                <li>{index+1}. {country}</li>
-              )
-              }
-            </ul>
+      <div className="flex flex-col gap-4">
+        <div className="h-[30vh] md:h-[60vh] lg:h-full w-full flex flex-col gap-4">
+          {!isMobile ? (
 
-            <ul>
-              {
-                countriesData.map((data)=>
-                  <div className="flex gap-2 items-start mb-2">
-                <div>
-                <BiCheck size={20}/>
-                </div>
-                <p className="text-subtitle">{data}</p>
-              </div>
-              )
-              }
-            </ul>
+          <ReactFitty className="text-headline">
+            TOP 10 CROSSBORDER INVESTORS COUNTRIES
+          </ReactFitty>
+          ) : (
+            <ReactFitty className="text-headline">
+            TOP 10 CROSSBORDER <br /> INVESTORS COUNTRIES
+          </ReactFitty>
+          )}
+          <div className="h-full w-full p-4 border border-gray-300 rounded-lg bg-white">
+            <img
+              src={WorldMap}
+              alt="world-map"
+              className="w-full h-full object-contain   rounded-lg"
+            />
+          </div>
         </div>
+        {/* <ul className="">
+          {countries.map((country, index) => (
+            <li>
+              {index + 1}. {country}
+            </li>
+          ))}
+        </ul> */}
+
+        <ul>
+          {countriesData.map((data) => (
+            <div className="flex gap-2 items-start mb-2">
+              <div>
+                <BiCheck size={20} />
+              </div>
+              <p className="text-subtitle">{data}</p>
+            </div>
+          ))}
+        </ul>
+      </div>
       {/* <p className="text-headline">
         TOTAL DUBAI REAL ESTATE SALES VALUE & VOLUME{" "}
       </p> */}
